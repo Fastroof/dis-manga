5690b3198d5bb4c83ecfacd62a400251bf1c5637
YuraShum <yura.shumel@gmail.com>
2023-03-26 15:39:29 +0300

    create swagger documentation

--- src/src/main/java/com/fastroof/ftpr/FtprApplication.java
+++ src/src/main/java/com/fastroof/ftpr/FtprApplication.java
@@ -2,8 +2,10 @@ package com.fastroof.ftpr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class FtprApplication {
 
 	public static void main(String[] args) {
