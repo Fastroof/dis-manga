4d5c884b8c84922ea89974fab3049a765d2a6390
Oleksandr <olieksandr.volovik@gmail.com>
2023-03-27 01:13:31 +0300

    File Storage service is now capable of uploading files to Google drive API

--- src/services/file-storage/src/main/java/com/fastroof/imagestorage/controller/RestController.java
+++ src/services/file-storage/src/main/java/com/fastroof/imagestorage/controller/RestController.java
@@ -2,15 +2,31 @@ package com.fastroof.imagestorage.controller;
 
 import com.fastroof.imagestorage.entity.ProductImage;
 import com.fastroof.imagestorage.repository.ProductImageRepository;
-import okhttp3.*;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import com.google.api.services.drive.model.Permission;
 import okhttp3.RequestBody;
+import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Optional;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,6 +35,113 @@ import java.util.regex.Pattern;
 public class RestController {
 
     private final ProductImageRepository productImageRepository;
+
+    /**
+     * Application name.
+     */
+    private static final String APPLICATION_NAME = "dis-manga";
+
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final Set<String> SCOPES = DriveScopes.all();
+
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+            throws IOException, GeneralSecurityException {
+        String serviceAccount = "dis-manga@dis-manga.iam.gserviceaccount.com";
+        InputStream inputStream = new FileInputStream("D:\\key.p12");
+
+        return new GoogleCredential.Builder()
+                .setTransport(HTTP_TRANSPORT)
+                .setJsonFactory(JSON_FACTORY)
+                .setServiceAccountId(serviceAccount)
+                .setServiceAccountPrivateKeyFromP12File(inputStream)
+                .setServiceAccountScopes(SCOPES)
+                .build();
+    }
+
+    @GetMapping("/files")
+    public List<File> getFiles() throws GeneralSecurityException, IOException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        FileList result = service.files().list()
+                .setPageSize(10)
+                .setFields("nextPageToken, files(id, name, webViewLink, webContentLink)")
+                .execute();
+
+        List<File> files = result.getFiles();
+
+        if (files == null || files.isEmpty()) {
+            return Collections.emptyList();
+        } else {
+            return files;
+        }
+    }
+
+    @PostMapping("/files")
+    public String postFile(@RequestParam MultipartFile file)
+            throws GeneralSecurityException, IOException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        java.io.File fileToUpload = convertMultipartFileToFile(file);
+
+        File fileMetadata = new File();
+        fileMetadata.setName(file.getOriginalFilename());
+        FileContent mediaContent = new FileContent("*/*", fileToUpload);
+
+        try {
+            File uploadedFile = service
+                    .files()
+                    .create(fileMetadata, mediaContent)
+                    .setFields("id")
+                    .execute();
+
+            Permission anyoneCanReadPermission = new Permission();
+            anyoneCanReadPermission.setType("anyone");
+            anyoneCanReadPermission.setRole("reader");
+
+            service
+                    .permissions()
+                    .create(uploadedFile.getId(), anyoneCanReadPermission)
+                    .execute();
+
+            return uploadedFile.getId();
+        } catch (GoogleJsonResponseException e) {
+            return e.getMessage();
+        }
+    }
+
+
+    private java.io.File convertMultipartFileToFile(MultipartFile file) throws IOException {
+        java.io.File convertedFile = new java.io.File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convertedFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convertedFile;
+    }
+
+
     private String status = "OK";
 
     @Autowired
