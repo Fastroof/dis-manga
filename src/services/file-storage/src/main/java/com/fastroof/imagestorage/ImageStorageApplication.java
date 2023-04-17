264121a1baac03f6d97e0a9168da083025005857
Oleksandr <olieksandr.volovik@gmail.com>
2023-04-17 20:13:31 +0300

    File storage service: add javadocs

--- src/services/file-storage/src/main/java/com/fastroof/imagestorage/ImageStorageApplication.java
+++ src/services/file-storage/src/main/java/com/fastroof/imagestorage/ImageStorageApplication.java
@@ -3,9 +3,17 @@ package com.fastroof.imagestorage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The ImageStorageApplication Class.
+ */
 @SpringBootApplication
 public class ImageStorageApplication extends SpringApplication{
 
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(ImageStorageApplication.class, args);
     }
